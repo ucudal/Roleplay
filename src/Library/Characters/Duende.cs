d7ae20288acf6c8880787c8e9cbd81991dd8cd20
Pablo Méndez <valentino.mendez@correo.ucu.edu.uy>
2022-04-26 21:01:33 -0300

    Add metodos Get

--- src/Library/Characters/Duende.cs
+++ src/Library/Characters/Duende.cs
@@ -5,40 +5,67 @@ namespace Library {
 
     public class Duende {
 
-        private int HP { get; set; }
-        private int Daño { get; set; }
-        private int Resistencia { get; set; }
-        public int VidaInicial { get; set; }
-        List<Item> ListadeItems = new List<Item>();
+        public string Nombre { get; private set; }
+        public int Ataque { get; private set; } = 0;
+        public int Defensa { get; private set; } = 0;
+        public List<Item> Items { get; private set; } = new List<Item>();
+        public int HP { get; set; } = 0;
+        public int VidaInicial { get; private set; } = 0;
+        public int DañoTotal { get; private set; }
+        public int DefensaTotal { get; private set; }
 
-        public Duende (int hp, int daño, int resistencia, int vidainicial) {
+        public Duende (string nombre, int ataque, int defensa, int hp, int vidainicial, int dañototal) {
 
+            this.Nombre = nombre;
+            this.Ataque = ataque;
+            this.Defensa = defensa;
             this.HP = hp;
-            this.Daño = daño;
-            this.Resistencia = resistencia;
             this.VidaInicial = vidainicial;
+            this.DañoTotal = dañototal;
+        }
+
+        public int ObtenerAtaque() {
+            foreach (Item item in this.Items)
+            {
+                this.DañoTotal = this.Ataque + item.Daño;
+            }
+            return DañoTotal;
+        }
+
+        public int ObtenerDefensa() {
+            foreach (Item item in this.Items)
+            {
+                this.DefensaTotal = this.Defensa + item.Defensa;
+            }
+            return DefensaTotal;
         }
 
         public void Atacar (Duende duende) {
-            foreach (Item item in this.ListadeItems) {
-                item.Desgaste();
-                this.Daño = this.Daño + item.Daño;
+            foreach (Item item in this.Items) {
+
+                if (!item.EstaRoto()) {
+                    item.Desgaste();
+                    this.Ataque = this.Ataque + item.Daño;
+                } else {
+                    this.DesequiparItem(item);
+                }
+                
             }
-            duende.HP = duende.HP - this.Daño;
+            duende.HP = duende.HP - this.Ataque;
         }
 
         public void EquiparItem (Item item)
         {
-            if (!this.ListadeItems.Contains(item)) {
+            if (!this.Items.Contains(item)) {
 
-                this.ListadeItems.Add(item); 
+                this.Items.Add(item); 
             } 
         }
         public void DesequiparItem (Item item)
         {
-            if (this.ListadeItems.Contains(item)) {
-                this.ListadeItems.Remove(item); 
-                
+            if (this.Items.Contains(item)) {
+
+                this.Items.Remove(item); 
             } 
         }
     
