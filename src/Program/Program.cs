6a76178e20ddada209b83b18e407353d374806e4
SanFerri <santiago.ferraro@correo.ucu.edu.uy>
2022-04-30 21:48:41 -0300

    Merge branch 'master' of https://github.com/ucudal/Roleplay

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,13 +7,27 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            // Un personaje
+            Elf Gandalf = new Elf("Gandalf", 120, 760, 30);
+            // Dos items
             Item Sword = new Item(30, 0, 20);
             Item Orb = new Item(30, 0, 20);
-            Elf Gandalf = new Elf("Gandalf", 120, 300, 30);
             Gandalf.AddItem(Sword);
             Gandalf.AddItem(Orb);
-            Undead Aainz = new Undead("Aainz", 0, 300, 20); 
-            Gandalf.Attack(Aainz);
+            // Un personaje
+            Undead Aainz = new Undead("Aainz", 80, 980, 20); 
+            // Dos items
+            Item BoneBow = new Item(25, 5, 25);
+            Item Scythe = new Item(35, 0, 15);
+            Aainz.AddItem(BoneBow);
+            Aainz.AddItem(Scythe);
+            // Un personaje
+            Dwarf Goliath = new Dwarf("Goliath", 100, 850, 30);
+            // Dos items
+            Item Hammer = new Item(50, 10, 5);
+            Item Spear = new Item(30, 0, 25);
+            Goliath.AddItem(Hammer);
+            Goliath.AddItem(Spear);
         }
     }
 }
